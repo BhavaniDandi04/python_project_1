e10ef1bb0aba7d5432ae180332c532912d7e5f3f
bhavanidandi04 <bhavanidandi@indiumsoft.com>
2024-08-02 15:17:58 +0530

    branch2

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,10 @@
 # tests/test_calculator.py
-
+import sys 
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'src')))
 import pytest
 from src.calculator import Calculator
+
 
 def test_add():
     assert Calculator.add(3, 2) == 5
